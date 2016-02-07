8d7540863c15380e86ae5cb99814a50393915046
eritikass <eritikass@gmail.com>
2016-02-07 20:01:16 +0200

    section: validation

--- client/user/login.js
+++ client/user/login.js
@@ -3,22 +3,74 @@
  */
 
 Template.login.events({
-    'submit form': function(event){
+    'submit form': function (event) {
         event.preventDefault();
-        var email = $('[name=email]').val();
-        var password = $('[name=password]').val();
-        Meteor.loginWithPassword(email, password, function(error) {
-            if(error){
-                alert(error.reason);
-            } else {
-                var currentRoute = Router.current().route.getName();
-
-                if (currentRoute == "login") {
-                    Router.go("home");
-                }
 
 
-            }
-        });
+        /*
+         var email = $('[name=email]').val();
+         var password = $('[name=password]').val();
+         Meteor.loginWithPassword(email, password, function(error) {
+         if(error){
+         alert(error.reason);
+         } else {
+         var currentRoute = Router.current().route.getName();
+
+         if (currentRoute == "login") {
+         Router.go("home");
+         }
+
+
+         }
+         });
+         */
     }
+});
+
+Template.login.onCreated(function () {
+    console.log("The 'login' template was just created.", this);
+});
+
+Template.login.onRendered(function () {
+    console.log("The 'login' template was just rendered.");
+
+    var validator = $('.login').validate({
+        submitHandler: function(event){
+            console.log("You just submitted the 'login' form.");
+
+            var email = $('[name=email]').val();
+            var password = $('[name=password]').val();
+            Meteor.loginWithPassword(email, password, function(error) {
+                if(error){
+                    console.log('error', error);
+                    if(error.reason == "User not found"){
+                        validator.showErrors({
+                            email: error.reason
+                        });
+                    }
+                    if(error.reason == "Incorrect password"){
+                        validator.showErrors({
+                            password: error.reason
+                        });
+                    }
+                } else {
+                    var currentRoute = Router.current().route.getName();
+
+                    if (currentRoute == "login") {
+                        Router.go("home");
+                    }
+
+
+                }
+            });
+
+
+        }
+    });
+
+
+});
+
+Template.login.onDestroyed(function () {
+    console.log("The 'login' template was just destroyed.");
 });
--- client/user/register.js
+++ client/user/register.js
@@ -5,6 +5,7 @@
 Template.register.events({
     'submit form': function () {
         event.preventDefault();
+/*
         var email = $('[name=email]').val();
         var password = $('[name=password]').val();
 
@@ -18,6 +19,42 @@ Template.register.events({
                 Router.go("home"); // Redirect user if registration succeeds
             }
         });
-
+*/
     }
+});
+
+
+
+Template.register.onRendered(function(){
+    var validator = $('.register').validate({
+        submitHandler: function(event){
+            var email = $('[name=email]').val();
+            var password = $('[name=password]').val();
+            Accounts.createUser({
+                email: email,
+                password: password
+            }, function(error){
+                if(error){
+                    console.log('error', error);
+                    if(error.reason == "User not found"){
+                        validator.showErrors({
+                            email: error.reason
+                        });
+                    }
+                    if(error.reason == "Incorrect password"){
+                        validator.showErrors({
+                            password: error.reason
+                        });
+                    }
+                    if(error.reason == "Email already exists."){
+                        validator.showErrors({
+                            email: "That email already belongs to a registered user."
+                        });
+                    }
+                } else {
+                    Router.go("home");
+                }
+            });
+        }
+    });
 });
